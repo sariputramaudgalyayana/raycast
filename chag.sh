89d1fb27284d99ec5a4fa1003fd34137ab107798
Ssriputra Maudgalyayana <sariputramaudgalyayana@gmail.com>
2026-04-08 18:10:21 -0400

    Add Raycast script to display Yom Tov info via israelite CLI

---
+++ chag.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# @raycast.schemaVersion 1
+# @raycast.title Chag
+# @raycast.mode fullOutput
+# @raycast.packageName My Scripts
+# @raycast.description See Yom Tov info
+# @raycast.icon 🕍
+
+# Load zsh environment (aliases, PATH, etc.)
+source ~/.zshrc
+
+israelite chag -v
